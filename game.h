be8e3f4e6eeca0a4c0ed5fd50eb204cc93c10944
Justin Dang <dangjustintime@gmail.com>
2017-06-02 11:42:04 -0400

    made files for game loop and party class

---
+++ game.h
@@ -0,0 +1,25 @@
+//game.h
+//includes game struct and basic game loop
+
+//need to link jobs.cpp,
+#include "chararcter.h"
+#include "items.h"
+
+
+struct Game {
+	public:
+		//constructor
+		Game();
+	
+	
+	
+	private:
+		//member functions
+		void GetFPS();
+		
+		
+		bool gameRun;
+
+
+
+}
---
+++ party.h
@@ -0,0 +1,15 @@
+//party.h
+//party struct for both player and foe
+#include "character.h"
+
+
+struct Party {
+
+	//constructor
+	Party();
+	//max num for party is six
+	character [5];
+
+
+
+}
